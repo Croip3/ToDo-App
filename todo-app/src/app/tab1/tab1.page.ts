80373b62c8fc80e0026a65e457a8e0c827ba5ca5
Croip3 <croip3@gmail.com>
2021-06-14 19:16:54 +0200

    get request works, array undifiend

--- todo-app/src/app/tab1/tab1.page.ts
+++ todo-app/src/app/tab1/tab1.page.ts
@@ -2,6 +2,7 @@ import {Component, Input} from '@angular/core';
 import {AddTodoListPage} from '../add-todo-list/add-todo-list.page';
 import {ModalController} from '@ionic/angular';
 import { SingleTodoPage } from '../single-todo/single-todo.page';
+import { HttpClient } from '@angular/common/http';
 
 export interface TodoList {
   id: number;
@@ -66,14 +67,44 @@ export class Tab1Page {
 
   @Input() public todoLists: TodoList[];
 
-  public mainList: TodoList;
+  //public mainList: TodoList;
+  public mainList: any;
   public otherLists: TodoList[];
+  todoList: any[];
+
+  private canRender = false;
 
   constructor(
     private modalController: ModalController,
+    private http: HttpClient
   ) {
-    this.mainList = Tab1Page.TODO_LISTS.find(({mainList}) => mainList);
-    this.otherLists = Tab1Page.TODO_LISTS.filter(({mainList}) => !mainList);
+    //this.mainList = Tab1Page.TODO_LISTS.find(({mainList}) => mainList);
+    //this.otherLists = Tab1Page.TODO_LISTS.filter(({mainList}) => !mainList);
+
+    //this.mainList = Tab1Page.TODO_LISTS.find(({mainList}) => mainList);
+
+    this.getData();
+    console.log("hi");
+  }
+
+  async test(){
+    await this.getData();
+  }
+
+  async getData(){
+    this.http.get('../../assets/data/todo.json').subscribe(
+        (res)=>{
+            console.log(res['allTodos']);
+            console.log(res);
+            this.mainList = res['allTodos'];
+            //this.mainList = this.todoList;
+            console.log('TESA:' + this.todoList);
+
+            //this.mainList = Tab1Page.res.find(({mainList}) => mainList);
+            console.log(this.mainList);
+
+            this.canRender =  true;
+        });
   }
 
   async presentModal(): Promise<void> {
