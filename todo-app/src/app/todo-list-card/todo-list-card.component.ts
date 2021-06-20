3e4131c1143552ff233af9e936da5e2147acd37a
Konrad Fritsche <fritsche@nextbike.com>
2021-06-20 23:27:31 +0200

    adjustments to todo-list-page | rework todo-list card component

--- todo-app/src/app/todo-list-card/todo-list-card.component.ts
+++ todo-app/src/app/todo-list-card/todo-list-card.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import { Router } from '@angular/router';
+import {Todo, TodoListWithTodos} from "../interface/Todo";
 
 @Component({
   selector: 'app-todo-list-card',
@@ -8,35 +9,48 @@ import { Router } from '@angular/router';
 })
 export class TodoListCardComponent implements OnInit {
 
-  @Input() public title: string;
-  @Input() public totalTasks: number;
-  @Input() public resolvedTasks: number;
-  @Input() public progress: number;
-  @Input() public mainList: boolean;
-  @Input() public color: string;
+  @Input() public todoList: TodoListWithTodos;
 
-  @Input() public description: string;
-  @Input() public id: number;
-  @Input() public date: string;
-  @Input() public startTime: string;
-  @Input() public endTime: string;
-
+  public description: string;
+  public progress: number;
 
   constructor(private router: Router) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.description = this.getDescription(this.todoList.todos);
+    this.progress = this.getProgress(this.todoList.todos);
+  }
 
   //navigation to detail page
   test(){
-    const navigationExtras = {
-      queryParams: {
-        title: this.title,
-        progress: this.progress,
-        color: this.color
-      }
-    };
-
-    this.router.navigate(['single-todo'], navigationExtras);
+    // const navigationExtras = {
+    //   queryParams: {
+    //     title: this.title,
+    //     progress: this.progress,
+    //     color: this.color
+    //   }
+    // };
+    //
+    // this.router.navigate(['single-todo'], navigationExtras);
   }
 
+  private getDescription(todos: Todo[]): string {
+    const finishedTodos = todos.filter(todo => todo.finished).length;
+    const totalTodos = todos.length;
+
+    console.log(typeof totalTodos)
+
+    if (totalTodos === 0) {
+      return 'Tap to add new task';
+    }
+
+    return `${finishedTodos} / ${totalTodos}`;
+  }
+
+  private getProgress(todos: Todo[]): number {
+    const finishedTodos = todos.filter(todo => todo.finished).length;
+    const totalTodos = todos.length;
+
+    return finishedTodos / totalTodos;
+  }
 }
