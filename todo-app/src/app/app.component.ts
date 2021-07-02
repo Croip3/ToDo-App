44106be41c6fe55b10a5eba2d789643c9037761c
Christopher <37404705+Croip3@users.noreply.github.com>
2021-07-02 13:51:47 +0200

    first splash test

--- todo-app/src/app/app.component.ts
+++ todo-app/src/app/app.component.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
-import {TodoListService} from "./service/todo-list.service";
-
+import { TodoListService } from './service/todo-list.service';
+import { SplashScreen } from '@capacitor/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
+
+/*SplashScreen.show({
+  autoHide: false
+});*/
 export class AppComponent {
   constructor(private todoListService: TodoListService) {
-    this.todoListService.initTodoLists()
-      .subscribe();
+    this.todoListService.initTodoLists().subscribe();
+  }
+
+  // eslint-disable-next-line @angular-eslint/use-lifecycle-interface
+  ngOnInit() {
+    SplashScreen.show({
+      autoHide: false,
+    });
   }
 }
